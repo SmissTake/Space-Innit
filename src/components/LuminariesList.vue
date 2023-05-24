23a8d96bf61fc4f5a47f5d2dca768ee2cc36cf2d
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-24 11:17:14 +0200

    added luminary card, not yet image

--- src/components/LuminariesList.vue
+++ src/components/LuminariesList.vue
@@ -3,7 +3,7 @@
     <h2>Luminaries List</h2>
     <ul>
       <li v-for="luminary in luminaries" :key="luminary.id">
-        {{ luminary.name }}
+        <LuminaryCard :luminary="luminary" />
       </li>
     </ul>
   </div>
@@ -13,17 +13,19 @@
 import { defineComponent } from 'vue'
 import { useLuminariesStore } from '@/store/luminaries'
 import Luminary from '@/models/luminary'
+import LuminaryCard from './LuminaryCard.vue'
 
 export default defineComponent({
-  name: 'LuminariesList',
-  data() {
-    return {
-      luminaries: [] as Luminary[],
-    }
-  },
-  async created() {
-    const store = useLuminariesStore()
-    this.luminaries = store.luminaries;
-  },
+    name: "LuminariesList",
+    data() {
+        return {
+            luminaries: [] as Luminary[],
+        };
+    },
+    async created() {
+        const store = useLuminariesStore();
+        this.luminaries = store.luminaries;
+    },
+    components: { LuminaryCard }
 })
 </script>
---
+++ src/components/LuminaryCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card>
+    <v-img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Star%2A.svg/1200px-Star%2A.svg.png" height="200"></v-img>
+    <v-card-title>{{ luminary.name }}</v-card-title>
+    <v-card-subtitle>{{ luminary.englishName }}</v-card-subtitle>
+    <v-card-text>
+      <p>Semimajor Axis: {{ luminary.semimajorAxis }}</p>
+      <p>Perihelion: {{ luminary.perihelion }}</p>
+      <p>Aphelion: {{ luminary.aphelion }}</p>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary" @click="$emit('view-details', luminary)">
+        View Details
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Luminary from '@/models/luminary'
+
+console.log(process.env.VUE_APP_UNSPLASH_ACCESS_KEY);
+
+export default defineComponent({
+  name: 'LuminaryCard',
+  props: {
+    luminary: {
+      type: Object as () => Luminary,
+      required: true,
+    },
+  },
+  computed: {
+    // imageUrl(): string {
+    //   return `https://api.unsplash.com/search/photos?query={${this.luminary.name}}&client_id={${process.env.VUE_APP_UNSPLASH_ACCESS_KEY}}`
+    // },
+  },
+})
+</script>
