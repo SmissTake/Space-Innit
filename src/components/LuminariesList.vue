210e7bf5a91ac8c10654b122610a78c27be02391
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-24 11:51:32 +0200

    added style do display flexé

--- src/components/LuminariesList.vue
+++ src/components/LuminariesList.vue
@@ -1,31 +1,39 @@
 <template>
   <div>
     <h2>Luminaries List</h2>
-    <ul>
-      <li v-for="luminary in luminaries" :key="luminary.id">
+    <v-row
+      gutter="20">
+      <v-col
+        v-for="luminary in luminaries"
+        :key="luminary.id"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3"
+      >
         <LuminaryCard :luminary="luminary" />
-      </li>
-    </ul>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import { useLuminariesStore } from '@/store/luminaries'
-import Luminary from '@/models/luminary'
-import LuminaryCard from './LuminaryCard.vue'
+import { defineComponent } from "vue";
+import { useLuminariesStore } from "@/store/luminaries";
+import Luminary from "@/models/luminary";
+import LuminaryCard from "./LuminaryCard.vue";
 
 export default defineComponent({
-    name: "LuminariesList",
-    data() {
-        return {
-            luminaries: [] as Luminary[],
-        };
-    },
-    async created() {
-        const store = useLuminariesStore();
-        this.luminaries = store.luminaries;
-    },
-    components: { LuminaryCard }
-})
+  name: "LuminariesList",
+  data() {
+    return {
+      luminaries: [] as Luminary[],
+    };
+  },
+  async created() {
+    const store = useLuminariesStore();
+    this.luminaries = store.luminaries;
+  },
+  components: { LuminaryCard },
+});
 </script>
