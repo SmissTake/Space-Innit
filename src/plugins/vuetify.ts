b4cb71fdc811d692b2fcd9d412f8ea19ed04afd9
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-24 12:15:15 +0200

    added custom dark & light theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,18 +9,47 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, ThemeDefinition } from 'vuetify'
+
+const customDark: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#13293D',
+    surface: '#13293D',
+    primary: '#006494',
+    'primary-darken-1': '#247BA0',
+    secondary: '#1B98E0',
+    'secondary-darken-1': '#E8F1F2',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+
+const customLight: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    primary: '#006494',
+    'primary-darken-1': '#247BA0',
+    secondary: '#1B98E0',
+    'secondary-darken-1': '#E8F1F2',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: 'light',
     themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+      dark: customDark,
+      light: customLight,
     },
   },
 })
