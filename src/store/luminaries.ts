674915937262bf6385ed4a927a7dd6788dbe4749
Gwion Tangi <lil.smiss7@gmail.com>
2023-05-24 10:36:46 +0200

    added luminaries to store

---
+++ src/store/luminaries.ts
@@ -0,0 +1,24 @@
+// Utilities
+import { defineStore } from 'pinia'
+// Types
+import Luminary from '@/models/luminary'
+
+export const useLuminariesStore = defineStore('app', {
+  state: () => ({
+    lumniaries: [] as Luminary[],
+  }),
+  actions: {
+    fetchLumniaries() {
+      fetch('https://api.le-systeme-solaire.net/rest/bodies/')
+        .then((response) => response.json())
+        .then((data) => {
+          data.bodies.forEach((luminary: Luminary) => {
+            this.addLuminary(luminary)
+          })
+        })
+    },
+    addLuminary(luminary: Luminary) {
+      this.lumniaries.push(luminary)
+    }
+  }
+})
