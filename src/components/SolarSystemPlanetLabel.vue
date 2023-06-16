0525a02b3eb5e0c270bac2d42e98f982b4d6d1ce
Gwion Tangi <lil.smiss7@gmail.com>
2023-06-16 10:13:11 +0200

    made solar system display, nice view up there

---
+++ src/components/SolarSystemPlanetLabel.vue
@@ -0,0 +1,37 @@
+<template>
+  <input :class='["planet"+id]' :id='planetClass' name='planet' type='radio'>
+  <label :class='["menu ", planetClass]' :for='planetClass'>
+    <div class='preview'></div>
+    <div class='info'>
+      <h2>
+        <div class='pip'></div>
+        {{ planet.name }}
+      </h2>
+      <h3>{{ id }}</h3>
+    </div>
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Luminary from '@/models/luminary';
+
+export default defineComponent({
+  name: 'SolarSystemPlanetLabel',
+  props: {
+    planet: {
+      type: Object as () => Luminary,
+      required: true
+    },
+    id: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    planetClass(): string {
+      return this.planet.englishName.toLowerCase();
+    }
+  }
+});
+</script>
