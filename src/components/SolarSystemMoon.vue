9b2c8afc9f2cb68bbe908bfd2d9bc5626e0e69a1
Gwion Tangi <lil.smiss7@gmail.com>
2023-06-18 15:48:52 +0200

    added moons

---
+++ src/components/SolarSystemMoon.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="['moon ', planetClass]">
+    <h3>Moon</h3>
+    <h2>
+      {{ planet.name }}
+    </h2>
+  </div>
+  <div :class="['trajectory ', planetClass]"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Luminary from "@/models/luminary";
+
+export default defineComponent({
+  name: "SolarSystemMoon",
+  props: {
+    planet: {
+      type: Object as () => Luminary,
+      required: true,
+    },
+  },
+  computed: {
+    planetClass(): string {
+      return this.planet.englishName.toLowerCase();
+    },
+  },
+});
+</script>
--- src/components/SolarSystemPlanetVisual.vue
+++ src/components/SolarSystemPlanetVisual.vue
@@ -1,12 +1,16 @@
 <template>
   <div class='solar_systm'>
     <div :class='["planet ", planetClass]'>
+      <template v-if="moons.length > 0">
+        <template v-for="(moon, index) in moons" :key="index">
+          <SolarSystemMoon :planet="moon"/>
+        </template>
+      </template>
       <div :class='["planet_description ", planetClass]'>
         <h2>Planet</h2>
         <h1>{{ planet.name }}</h1>
-        <p>The closest planet to the sun. It circles the sun faster than all the other planets, which is why Romans named it after their swift-footed messenger god.</p>
       </div>
-      <div class='overlay'></div>
+      <!-- <div class='overlay'></div> -->
     </div>
   </div>
 </template>
@@ -14,6 +18,10 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Luminary from '@/models/luminary';
+import { useLuminariesStore } from '@/store/luminaries';
+import SolarSystemMoon from '@/components/SolarSystemMoon.vue';
+
+const store = useLuminariesStore();
 
 export default defineComponent({
   name: 'SolarSystemPlanetVisual',
@@ -23,9 +31,23 @@ export default defineComponent({
       required: true
     },
   },
+  components: {
+    SolarSystemMoon
+  },
+  data() {
+    return {
+      luminaries: [] as Luminary[],
+    };
+  },
+  created() {
+    this.luminaries = store.luminaries;
+  },
   computed: {
     planetClass(): string {
       return this.planet.englishName.toLowerCase();
+    },
+    moons(): Luminary[] {
+      return this.luminaries.filter((luminary) => luminary.aroundPlanet?.planet === this.planet.id);
     }
   }
 });
