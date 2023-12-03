51f78b10c9b4cf7167baf00cf1b6057a0b4aae1d
Gwion Tangi <lil.smiss7@gmail.com>
2023-12-03 22:34:07 +0100

    added body type filter

---
+++ src/components/LuminariesFilters.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-radio-group
+      v-model="filters.type"
+      @change="$emit('type', filters.type)"
+      >
+      <v-radio label="Planets" value="Planet" />
+      <v-radio label="Moons" value="Moon" />
+      <v-radio label="Asteroids" value="Asteroid" />
+      <v-radio label="Comets" value="Comet" />
+    </v-radio-group>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "LuminariesFilters",
+  data() {
+    return {
+      filters: {
+        type: "",
+      },
+    };
+  },
+  emits: {
+    type: (type: string) => true,
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <LuminariesSearch @search="search"/>
+    <LuminariesFilters @type="type" />
     <LuminariesList :luminaries-set="searchedLuminaries"/>
   </div>
 </template>
@@ -9,6 +10,7 @@
 import { defineComponent } from 'vue'
 import LuminariesList from '@/components/LuminariesList.vue'
 import LuminariesSearch from '@/components/LuminariesSearch.vue'
+import LuminariesFilters from '@/components/LuminariesFilters.vue' // Import the LuminariesFilters component
 import { useLuminariesStore } from "@/store/luminaries";
 import Luminary from '@/models/luminary';
 
@@ -19,13 +21,15 @@ export default defineComponent({
   components: {
     LuminariesList,
     LuminariesSearch,
-  },
+    LuminariesFilters
+},
   data() {
     return {
       isLoading: store.$state.isLoading,
       luminaries: [] as Luminary[],
       luminariesSearched : [] as Luminary[],
       searchString: '',
+      filteredType: ''
     }
   },
   async created() {
@@ -35,15 +39,23 @@ export default defineComponent({
   methods: {
     search(searchString: string) {
       this.searchString = searchString
-    }
+    },
+    type(filteredType: string) {
+      this.filteredType = filteredType
+    },
   },
   computed: {
     searchedLuminaries() {
       this.luminariesSearched = this.luminaries.filter((luminary) => {
         return luminary.name.toLowerCase().includes(this.searchString.toLowerCase());
       });
+      if (this.filteredType) {
+        this.luminariesSearched = this.luminariesSearched.filter((luminary) => {
+          return luminary.bodyType === this.filteredType;
+        });
+      }
       return this.luminariesSearched;
-    }
+    },
   }
 })
 </script>
