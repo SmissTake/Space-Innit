3f39b812827356530b3ddc124ea2ad9501b0bb30
Gwion Tangi <lil.smiss7@gmail.com>
2023-12-03 22:53:32 +0100

    changed style

--- src/components/LuminariesFilters.vue
+++ src/components/LuminariesFilters.vue
@@ -1,18 +1,28 @@
 <template>
   <div>
-    <h3>
-      Type:
-    </h3>
-    <v-radio-group
-      v-model="filters.type"
-      @change="$emit('type', filters.type)"
-      inline
+    <v-expansion-panels
+        variant="popout"
+    >
+      <v-expansion-panel
+        title="Filter"
       >
-      <v-radio label="Planets" value="Planet" @click="uncheck('Planet')"/>
-      <v-radio label="Moons" value="Moon" @click="uncheck('Moon')"/>
-      <v-radio label="Asteroids" value="Asteroid" @click="uncheck('Asteroid')"/>
-      <v-radio label="Comets" value="Comet" @click="uncheck('Comet')"/>
-    </v-radio-group>
+        <v-expansion-panel-text>
+          <h3>
+            Type
+          </h3>
+          <v-radio-group
+            v-model="filters.type"
+            @change="$emit('type', filters.type)"
+            inline
+            >
+            <v-radio label="Planets" value="Planet" @click="uncheck('Planet')"/>
+            <v-radio label="Moons" value="Moon" @click="uncheck('Moon')"/>
+            <v-radio label="Asteroids" value="Asteroid" @click="uncheck('Asteroid')"/>
+            <v-radio label="Comets" value="Comet" @click="uncheck('Comet')"/>
+          </v-radio-group>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
   </div>
 </template>
 
